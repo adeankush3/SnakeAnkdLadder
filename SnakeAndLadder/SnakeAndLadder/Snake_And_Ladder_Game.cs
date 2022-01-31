cd59dd4bfa97aa10e676262f3f4dd065f73a00f0
ANKUSH RAMRAO ADE <49829045+adeankush3@users.noreply.github.com>
2022-01-31 18:53:16 +0530

    Update Snake_And_Ladder_Game.cs

--- SnakeAndLadder/SnakeAndLadder/Snake_And_Ladder_Game.cs
+++ SnakeAndLadder/SnakeAndLadder/Snake_And_Ladder_Game.cs
@@ -1,12 +1,48 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SnakeAndLadder
+namespace Snake_And_Ladder_Problem
 {
-    internal class Snake_And_Ladder_Game
+    public class Snake_And_Ladder_Game
     {
+        int player_pos = 0,count=0;
+        Random random = new Random();
+        public int DiceRoll()
+        {
+            int dice = random.Next(1, 7);
+            count++;
+            return dice;
+        }
+        public int Play()
+        {
+            const int LADDER = 1, SNAKE = 2;
+                int choice = random.Next(1, 3);
+                int diceoutcome = DiceRoll();
+                switch (choice)
+                {
+                    case LADDER:
+                    if (player_pos + diceoutcome <= 100)
+                    {
+                        player_pos += diceoutcome;
+                        Play();
+                    }
+                        break;
+                    case SNAKE:
+                        if(player_pos-diceoutcome > 0)
+                        player_pos -= diceoutcome;
+                        else
+                        {
+                            player_pos = 0;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            return player_pos;
+            }
+
     }
 }
